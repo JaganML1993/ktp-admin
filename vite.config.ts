d7907d98e8da0ead0a14ef7289451b0f71eca62b
jagan-fple <jagan@fple.in>
2025-06-15 08:49:07 +0530

    app name change

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
   },
-  base: '/jv-app/',
+  base: '/ktp-app/',
   build: {
-    outDir: 'dist/jv-app', // This is crucial!
+    outDir: 'dist/ktp-app', // This is crucial!
   },
 });
